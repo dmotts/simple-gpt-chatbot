95728adbe6bbcb18b22c437e9bf085610feeb78b
Daley Mottley <daley.mottley@hotmail.com>
2024-10-01 16:45:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("💬 Chatbot")
 st.write(
     "This is a simple chatbot that uses OpenAI's GPT-4o-mini model to generate responses. "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "You can also learn how to build this app step by step by following this [tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
